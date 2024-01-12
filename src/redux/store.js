fc0db989c4ae4d37b16ec58da3182bcebb3ef48c
Matt <mattgq2@gmail.com>
2024-01-12 07:53:06 -0700

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./reducers/books";
-import userReducer from "./reducers/user/user";
+import userReducer from "./reducers/user";
 
 export const store = configureStore({
     reducer: { books: booksReducer, user: userReducer }
